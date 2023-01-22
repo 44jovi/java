1a23fa61577405cd580da0e2c23d9aa185dfc8b4
Jovi Lau-Kwong <109252621+44jovi@users.noreply.github.com>
2023-01-22 09:18:32 +0000

    Create data_types.java

---
+++ data_types.java
@@ -0,0 +1,11 @@
+// Snippets
+
+public class Main {
+    public static void main(String[] args) {
+        int myNum = 1;
+        float myFloat = 1.1234567f; // without f, it would save as a double
+        char myCharacter = 'A';
+        boolean myBoolean = true;
+        String myString = 'How are you?'
+    }
+}
