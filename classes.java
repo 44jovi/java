d8a80b041bf5a7c1d44f1281a6f0629c383b5046
jovi <kwong.jovi@gmail.com>
2023-02-12 23:39:45 +0000

    create classes.java

---
+++ classes.java
@@ -0,0 +1,11 @@
+// Simple Java class example:
+
+class Product {
+  int price;
+  int getPrice() { // getter
+    return price;
+  }
+  void setPrice(int newPrice) { // setter
+    price = newPrice
+  }
+}
