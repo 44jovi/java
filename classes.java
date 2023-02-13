0cf95447b1618c65eeb089efd6de8cc54e5f19c8
jovi <kwong.jovi@gmail.com>
2023-02-13 23:19:06 +0000

    update(classes.java): add snippet re public classes

--- classes.java
+++ classes.java
@@ -5,7 +5,19 @@ class Product {
   int getPrice() { // getter
     return price;
   }
+  // 'void' keyword specifies that a method should not have a return value
   void setPrice(int newPrice) { // setter
     price = newPrice
+  }
+}
+
+// A public class is accessible by any other class within a Java package
+
+public class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main object1 = new Main();
+    System.out.printIn(object1).x;
   }
 }
